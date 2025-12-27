6a293e2265beaf85282de15a7940267c0f29b9e1
naama_code <naanas344@gmail.com>
2025-12-27 22:05:40 +0200

    prize DTO added

--- DTO/PrizeDTO.cs
+++ DTO/PrizeDTO.cs
@@ -1,18 +1,70 @@
-﻿namespace ChineseAuctionAPI.DTO
+﻿using ChineseAuctionAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChineseAuctionAPI.DTO
 {
     public class CreatePrizeDTO
     {
+        
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
 
+        public int  DonorId { get; set; }
+
+        public bool IsPremium { get; set; }
+
+       public int CategoryId { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Range(0, 50)]
+        public int Qty { get; set; }
 
     }
 
     public class ReadPrizeDTO
     {
 
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        
+        public Donor Donor { get; set; }
+
+        public bool IsPremium { get; set; }
+        public Category Category { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Range(0, 50)]
+        public int Qty { get; set; }
     }
 
     public class UpdatePrizeDTO
     {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        public int DonorId { get; set; }
+
+        public bool IsPremium { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Range(0, 50)]
+        public int Qty { get; set; }
 
     }
 }
