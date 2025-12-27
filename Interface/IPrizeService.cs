7c9faf14aef70e271e6c9b79da8fc6408174d059
naama_code <naanas344@gmail.com>
2025-12-27 22:26:26 +0200

    IPrizeService Interface added

--- Interface/IPrizeService.cs
+++ Interface/IPrizeService.cs
@@ -1,10 +1,18 @@
-﻿namespace ChineseAuctionAPI.Interface
+﻿using ChineseAuctionAPI.DTO;
+
+namespace ChineseAuctionAPI.Interface
 {
     public interface IPrizeService
     {
 
-        
+        public Task<IEnumerable<ReadPrizeDTO>> GetPrizes();
+        public Task AddPrize(CreatePrizeDTO prize);
+        public Task DeletePrize(int id);
+        public Task<ReadPrizeDTO> GetPrizeById(int id);
 
-        
+        public Task UpdatePrize(UpdatePrizeDTO prize);
+
+
+
     }
 }
