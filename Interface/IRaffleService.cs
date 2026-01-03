6c82fb63c16053c8b441afbca44a7dfc143d42b1
Lea2166 <lea202166@gmail.com>
2026-01-03 21:28:07 +0200

    adding Ruffle Service controller with checking

---
+++ Interface/IRaffleService.cs
@@ -0,0 +1,10 @@
+﻿using ChineseAuctionAPI.Models;
+
+namespace ChineseAuctionAPI.Interface
+{
+    public interface IRaffleService
+    {
+        public Task<Winner> PerformRaffle(int prizeId);
+
+    }
+}
