bcd78a225ed01b1778025ac66f8ee04f523818c1
Lea2166 <lea202166@gmail.com>
2026-01-12 16:20:19 +0200

    add prize

--- Program.cs
+++ Program.cs
@@ -1,20 +1,14 @@
-<<<<<<< Updated upstream
 
 using ChineseAuctionAPI.Data;
 using ChineseAuctionAPI.Interface;
 using ChineseAuctionAPI.Services;
 using Microsoft.EntityFrameworkCore;
-=======
-using ChineseAuctionAPI.Data;
-using ChineseAuctionAPI.Interface;
 using ChineseAuctionAPI.Middlewares;
 using ChineseAuctionAPI.Models.Exceptions;
 using ChineseAuctionAPI.Repositories;
-using ChineseAuctionAPI.Services;
 using ChineseAuctionAPI.Validations;
 using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Serilog;
@@ -23,63 +17,11 @@ using Swashbuckle.AspNetCore.Filters;
 using System.Security.Claims;
 using System.Text;
 
->>>>>>> Stashed changes
 
 namespace ChineseAuctionAPI
 {
     public class Program
     {
-<<<<<<< Updated upstream
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            // dbContext
-            builder.Services.AddDbContext<ChineseAuctionDBcontext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-            );
-
-            // db factory
-            builder.Services.AddSingleton<DbContextFactory>();
-
-            //Auto Mapper
-            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            // DI
-            builder.Services.AddScoped<IDonorRepo, DonorRepository>();
-            builder.Services.AddScoped<IDonorService, DonorService>();
-
-            
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
-}
-=======
-
-
         public static void Main(string[] args)
         {
 
@@ -296,4 +238,3 @@ namespace ChineseAuctionAPI
         }
     }
 }
->>>>>>> Stashed changes
