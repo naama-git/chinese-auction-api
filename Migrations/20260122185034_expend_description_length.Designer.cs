6ca12c31726ae9a9b0a705ea41c1ea1fd67a078a
naama_code <naanas344@gmail.com>
2026-01-22 20:51:45 +0200

    expend description length from 500 to 1000

--- Migrations/20260122185034_expend_description_length.Designer.cs
+++ Migrations/20260122185034_expend_description_length.Designer.cs
@@ -4,6 +4,7 @@ using ChineseAuctionAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChineseAuctionAPI.Migrations
 {
     [DbContext(typeof(ChineseAuctionDBcontext))]
-    partial class ChineseAuctionDBcontextModelSnapshot : ModelSnapshot
+    [Migration("20260122185034_expend_description_length")]
+    partial class expend_description_length
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
